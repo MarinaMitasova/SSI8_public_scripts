a72f80e96a371c53ed8201c54b3f0ee8c9e3ca2d
MarinaMitasova <marinamorozova_box@mail.ru>
2021-01-29 19:07:12 +0300

    otherByColumnsInTable

--- Lighthouse-1.0.js
+++ Lighthouse-1.0.js
@@ -1,13 +1,17 @@
-﻿function changeInputTextOrExclusive(opts){
+﻿function checkArguments(arg, opts){
+	if (arg.length == 0 || opts === undefined){throw "Функция должна принимать один аргумент в виде объекта."}
+	if (arg.length > 1){throw "Слишком много арументов для данной функции! \nФункция должна принимать только один аргумент в виде объекта."}
+	if (opts.ques === undefined){throw "Не задано имя вопроса `ques`!"}
+	if ($("#"+opts.ques+"_div").length == 0){throw "Вопрос " + opts.ques + " не найден на странице."}
+}
+
+function changeInputTextOrExclusive(opts){
 
     //ques:  - Обязательный
     //answLocation: default="inColumns"
 	
-	if (arguments.length == 0 || opts === undefined){throw "Функция должна принимать один аргумент в виде объекта."}
-	if (arguments.length > 1){throw "Ошибка:\nСлишком много арументов для данной функции! \nФункция должна принимать только один аргумент в виде объекта."}
+	checkArguments(arguments, opts);
 	var ques = opts.ques;
-	if (ques === undefined){throw "Не задано имя вопроса `ques`!"}
-	if ($("#"+ques+"_div").length == 0){throw "Вопрос " + ques + " не найден на странице."}
 
 	var quesDirection = opts.answLocation === undefined ? "inColumns" : opts.answLocation;
 	if (quesDirection != "inRows" && quesDirection != "inColumns"){
@@ -91,11 +95,8 @@ function checkInputTextOrExclusive(opts){
 	//inputsCheck (default="some") some / every
 	//continueIf - доступен для переопределения 
 
-	if (arguments.length == 0 || opts === undefined){throw "Функция должна принимать один аргумент в виде объекта."}
-	if (arguments.length > 1){throw "Ошибка:\nСлишком много арументов для данной функции! \nФункция должна принимать только один аргумент в виде объекта."}
+	checkArguments(arguments, opts);
 	var ques = opts.ques;
-	if (ques === undefined){throw "Не задано имя вопроса `ques`!"}
-	if ($("#"+ques+"_div").length == 0){throw "Вопрос " + ques + " не найден на странице."}
 	
 	var quesDirection = opts.answLocation === undefined ? "inColumns" : opts.answLocation;
 	if (quesDirection != "inRows" && quesDirection != "inColumns"){
@@ -171,4 +172,131 @@ function checkInputTextOrExclusive(opts){
 	})
 	
 	return err;
+}
+
+function otherByColumnsInTable(opts){
+
+	checkArguments(arguments, opts);
+	var ques = opts.ques;
+
+	if (opts.other === undefined || opts.other === ""){
+		throw "Номер строки-уточнения не задан или является пустой строкой!"
+	}
+	switch (Object.prototype.toString.call(opts.other)){
+		case "[object Number]":
+			var other = opts.other.toString();
+			break;
+		case "[object String]":	
+		default:
+			throw "Номер строки-уточнения `other` должен быть числом!"
+	}
+	var nameOther = opts.nameOther === undefined ? ques+"other" : opts.nameOther;
+	var otherLocation = opts.otherLocation === undefined ? "inColumns" : opts.otherLocation;
+	if (otherLocation != "inRows" && otherLocation != "inColumns"){
+		throw "Параметр otherLocation д.б. равен либо 'inRows', либо 'inColumns'(по умолчанию)!"
+	}
+	if (nameOther.toString() !== nameOther || nameOther === ""){
+		throw "Переменная nameOther должна быть строкой и \nсодержать в себе общую часть имени вопросов-уточнения!";
+	}
+
+	if (otherLocation == "inRows"){
+		$("div[id^='" + nameOther + "']").find("input[type='text'], textarea").each(function() {
+			$(this).parents("tr:first").hide();
+			if ($(this).val() == ""){
+				$(this).val("none");
+			}
+		});
+
+		$("#" + ques + "_div").find("input[type='checkbox'], input[type='radio']").each(function(i, answ) {
+			showOrHideOther1(answ);
+		});
+
+		$(document).on('lighthouseCheckboxChanged lighthouseRadioButtonChanged', function(event, graphicalObj, inputObj, bln) {
+			var reg = new RegExp(ques + "_.+");
+			if (reg.test(inputObj.id)){
+				showOrHideOther1(inputObj);
+			}
+		});
+
+		function showOrHideOther1(answ){
+			var id, r, c, rad, chb, elOther;
+			id = $(answ).attr('id');
+			if ($(answ).attr('type') == "checkbox"){
+				r = id.split('_')[1].replace("r", "");
+				c = id.split('_')[2].replace("c", "");
+				chb = "#" + ques + "_r" + other + "_c" + c;
+				elOther = $(chb);
+			}else{
+				r = id.split('_')[2];
+				c = id.split('_')[1].replace("c", "");
+				rad = "#" + ques + "_c" + c + "_" + other;
+				elOther = $(rad);
+			}
+			if (elOther.prop("checked")){
+				if (r == other){
+					$("#" + nameOther + "_div").show();
+					$("#" + nameOther + "_r" + c + "_c1").parents("tr:first").show();
+					if ($("#" + nameOther + "_r" + c + "_c1").val() == "none"){
+						$("#" + nameOther + "_r" + c + "_c1").val("");
+					}
+				}
+			}else{
+				$("#" + nameOther + "_r" + c + "_c1").parents("tr:first").hide();
+				$("#" + nameOther + "_r" + c + "_c1").val("none");
+			}
+			if ($("#" + nameOther + "_div").find("input[type='text'], textarea").not(":hidden").length == 0){
+				$("#" + nameOther + "_div").hide();
+			}
+		}
+	}else{
+		$("div[id^='" + nameOther + "']").find("input[type='text'], textarea").each(function() {
+			$(this).hide();
+			if ($(this).val() == ""){
+				$(this).val("none");
+			}
+		});
+
+		$("#" + ques + "_div").find("input[type='checkbox'], input[type='radio']").each(function(i, answ) {
+			showOrHideOther2(answ);
+		});
+
+		$(document).on('lighthouseCheckboxChanged lighthouseRadioButtonChanged', function(event, graphicalObj, inputObj, bln) {
+			console.log(inputObj.id, bln)
+			var reg = new RegExp(ques + "_.+");
+			if (reg.test(inputObj.id)){
+				showOrHideOther2(inputObj);
+			}
+		});
+
+		function showOrHideOther2(answ){
+			var id, r, c, rad, chb, elOther;
+			id = $(answ).attr('id');
+			if ($(answ).attr('type') == "checkbox"){
+				r = id.split('_')[1].replace("r", "");
+				c = id.split('_')[2].replace("c", "");
+				chb = "#" + ques + "_r" + other + "_c" + c;
+				elOther = $(chb);
+			}else{
+				r = id.split('_')[2];
+				c = id.split('_')[1].replace("c", "");
+				rad = "#" + ques + "_c" + c + "_" + other;
+				elOther = $(rad);
+			}
+			if (elOther.prop("checked")){
+				if (r == other){
+					$("#" + nameOther + "_div").show();
+					$("#" + nameOther + "_r1_c" + c).show();
+					if ($("#" + nameOther + "_r1_c" + c).val() == "none"){
+						$("#" + nameOther + "_r1_c" + c).val("");
+					}
+				}
+			}else{
+				$("#" + nameOther + "_r1_c" + c).hide();
+				$("#" + nameOther + "_r1_c" + c).val("none");
+			}
+			if ($("#" + nameOther + "_div").find("input[type='text'], textarea").not(":hidden").length == 0){
+				$("#" + nameOther + "_div").hide();
+			}
+		}
+	}
 }
